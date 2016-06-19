1be165aaaa2e32e0f831827a46d92a3b2519624b
Anthony Flynn <anthonymilesflynn@gmail.com>
2016-06-19 07:53:33 +0100

    added script to download reference stop data to the database

---
+++ php_scripts/stop_reference/server_script_stop_reference.php
@@ -0,0 +1,133 @@
+<?php
+
+// SET UP DATABASE CONNECTION /////////////////////////////////////////
+
+// Database access information:
+$database_type = 'pgsql';
+$server_ip = "";
+$database_name = "bus_data";
+$username = "";
+$password = "";
+
+
+// Create database connection:
+try {
+  $DBH = new PDO("$database_type:host=$server_ip;dbname=$database_name",$username,$password);
+}
+catch(PDOException $e) {
+  echo $e->getMessage();
+  echo "\n";
+  exit(1);
+}
+echo "Database connection successful";
+
+
+// SET UP STREAM WRAPPER //////////////////////////////////////////////
+
+class tflStreamWrapper {
+  protected $buff; // buffer to store any partial lines during parsing
+  
+  function stream_open($path, $mode, $options, &$opened_path) {
+    return true;
+  } // called immediately after wrapper initialised
+ 
+  function stream_write($data) { // caled when data written by cURL
+    $stop_data = explode("\n", $data); //creates array of stop data from stream
+    
+    // $buff contains the incomplete last line of data that was present when
+    // the last batch of data was written to the database (or nothing if there
+    // were no incomplete lines).  This is therefore prefixed to the first item
+    // in the $stop_data array
+    $stop_data[0] = $this->buff.$stop_data[0];
+ 
+    // Save the last line of this batch of data to the buffer (in case it is
+    // incomplete.  Will be prefixed to first item in next batch
+    $stop_data_count = count($stop_data);
+    $this->buff = $stop_data[$stop_data_count - 1];
+    unset($stop_data[$stop_data_count - 1]); //delete last item in $stop_data
+
+    $insert = "INSERT INTO stop_reference (stoppointname,stopid,stopcode1,stopcode2,stoppointtype,towards,bearing,stoppointindicator,stoppointstate,latitude,longitude) ";
+
+    $delete = "DELETE FROM stop_reference (stoppointname,stopid,stopcode1,stopcode2,stoppointtype,towards,bearing,stoppointindicator,stoppointstate,latitude,longitude) WHERE stopid=";
+ 
+    // For each stop_data item in turn:
+    for ($i=0; $i < count($stop_data); $i++) {
+      //remove characters from front and back ('[',']' and newline character)
+      $trimmed = trim($stop_data[$i], "[]\n\r"); 
+      $entry = explode(",", $trimmed); // split data into array
+
+      // To be of interest, the line must have 12 pieces of data, and should 
+      // exclude the URA Version array (must start with a '0')
+      
+      if(count($entry) == 12 && $entry[0] == 0) {
+          //modify strings to make compliant for database insertion
+	  modify_string($entry[1]);     	  
+	  modify_string($entry[2]);
+	  modify_string($entry[3]);
+	  modify_string($entry[4]);
+	  modify_string($entry[5]);
+	  modify_string($entry[6]);
+	  modify_string($entry[8]);
+
+	  // Set up string to insert values
+          $sql = $insert."values ($entry[1],$entry[2],$entry[3],$entry[4],$entry[5],$entry[6],$entry[7],$entry[8],$entry[9],$entry[10],$entry[11])";
+
+	  // Set up string to remove any existing data for the StopID
+	  $delete_entry = $delete."$entry[2]";
+	  
+	  try {
+	    $DEL = $GLOBALS['DBH']->prepare($delete_entry);
+	    $DEL->execute();
+ 	    $STH = $GLOBALS['DBH']->prepare($sql); // Prepares an SQL statement and returns a statement object
+	    $STH->execute(); // executes the prepared statement
+	  }
+	  catch(PDOException $e) {
+	    echo $e -> getMessage();
+	    echo "\n";
+	  }
+      }
+    }
+    return strlen($data);
+  }
+}
+
+// Register wrapper:
+stream_wrapper_register("tflStreamWrapper","tflStreamWrapper") 
+  or die("Failed to register protocol");
+
+// Open a file handler using the wrapper protocol
+$fp = fopen("tflStreamWrapper://tflStream","r+")
+  or die("Error opening wrapper file handler");
+
+// Replace double quotes with single quotes, and escape any single quotes within string
+function modify_string(&$str) {
+  $str = substr($str, 1, -1);
+  $str = $GLOBALS['DBH']->quote($str);
+}
+
+
+// SET UP cURL SESSION ////////////////////////////////////////////////
+
+// Initialise cURL session:
+$curl = curl_init(); 
+
+// Set cURL options:
+curl_setopt($curl, CURLOPT_URL, "http://countdown.api.tfl.gov.uk/interfaces/ura/stream_V1?StopAlso=true&ReturnList=StopPointName,StopID,StopCode1,StopCode2,StopPointType,Towards,Bearing,StopPointIndicator,StopPointState,Latitude,Longitude");
+
+curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST); // Digest authorisation
+
+curl_setopt($curl, CURLOPT_USERPWD, "y:"); // User details
+
+curl_setopt($curl, CURLOPT_FILE, $fp); // file pointer for output data
+
+curl_setopt($curl, CURLOPT_TIMEOUT, 99999999); // Long-lived connection
+
+
+// Start the cURL session (begin collecting data):
+curl_exec($curl);
+
+// Close the connection and file handler when finished:
+curl_close($curl);
+fclose($fp);
+
+?>
