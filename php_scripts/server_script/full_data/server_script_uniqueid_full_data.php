00c3edf0ce8b7648c93a8f3060dfb6cf636f4490
Anthony Flynn <anthonymilesflynn@gmail.com>
2016-07-19 21:13:28 +0100

    Added full data versions of server script to extract information from TfL API, adjusting the SQL query to correctly extract relevant uniqueids on script restart

---
+++ php_scripts/server_script/full_data/server_script_uniqueid_full_data.php
@@ -0,0 +1,23 @@
+<?php
+
+include '/data/individual_project/php/modules/stream_wrappers/UniqueidStreamWrapperClass.php';
+include '/data/individual_project/php/modules/HttpClientClass.php';
+include '/data/individual_project/php/modules/DatabaseClass.php';
+
+// Open up file handler user stream wrapper
+$fp = fopen("tflStreamWrapper://tflStream","r+")
+  or die("Error opening wrapper file handler");
+
+// Create new HTTP client
+$url = "http://countdown.api.tfl.gov.uk/interfaces/ura/stream_V1?"
+      ."ReturnList=StopID,VisitNumber,LineName,DirectionID,DestinationText,"
+      ."VehicleID,TripID,Registrationnumber,EstimatedTime,ExpireTime";
+
+$Http = new HttpClient($url, $fp);
+$Http->start_data_collection();
+
+// Close connection / file handler on conclusion
+$Http->close_connection();
+fclose($fp);
+
+?>
