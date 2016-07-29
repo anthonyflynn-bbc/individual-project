297849dc778d3fd760b5a3be90c8190a33235955
Anthony Flynn <anthonymilesflynn@gmail.com>
2016-07-29 09:28:03 +0100

    Changed access modifiers on class methods and attributes, minor changes to action on PDOException being raised.  Added phpunit tests

--- php_scripts/modules/DatabaseClass.php
+++ php_scripts/modules/DatabaseClass.php
@@ -1,38 +1,58 @@
 <?php
 
+// DatabaseClass.php
+// Anthony Miles Flynn
+// (27/07/16)
+// Forms a connection to the database specified in the constructor parameters
+// or connects to the default parameters if none provided
+
 class Database {
-  protected $DBH; // database connection
-  protected $database_type = 'pgsql';
-  protected $server_ip = "";
-  protected $database_name = "bus_data";
-  protected $username = "";
-  protected $password = "";
+  private $DBH; // PDO instance (database connection)
+  private $database_type;
+  private $server_ip;
+  private $database_name;
+  private $username;
+  private $password;
 
   // Constructor
-  function __construct() {
-    $this->connect();
+  public function __construct($database_type = "pgsql", 
+  	 	  	      $server_ip = "",
+  	   	       	      $database_name = "bus_data", 
+			      $username = "",
+		       	      $password = "") {
+    $this->database_type = $database_type;
+    $this->server_ip = $server_ip;
+    $this->database_name = $database_name;
+    $this->username = $username;
+    $this->password = $password;
+    $this->connect(); // call method to connect to database
   }
 
   // Function returns the database object connection
-  function get_connection() {
+  public function get_connection() {
     return $this->DBH;
   }
 
   // Function forms a connection to the bus_data database
   protected function connect() {
+    // form the data source name for the database
+    $dsn = "$this->database_type:host=$this->server_ip;"
+    	  ."dbname=$this->database_name"; 
+
+    // attempt to connect to the database
     try {
-      $this->DBH = new PDO("$this->database_type:host=$this->server_ip;dbname=$this->database_name",
-		            $this->username,$this->password);
+      $this->DBH = new PDO($dsn, $this->username, $this->password);
       return $this->DBH;
     }
     catch(PDOException $e) {
       echo $e->getMessage()."\n";
-      exit(1);
+      return;
     }
+    echo "Database connection successful.\n";
   }
 
-  // Function executes an sql statement & returns an array containing the response
-  function execute_sql($sql_statement) {
+  // Function executes an sql statement & returns an array containing response
+  public function execute_sql($sql_statement) {
     try {
       $database_obj = $this->DBH->prepare($sql_statement);
       $database_obj->execute(); // executes the prepared statement;
---
+++ php_scripts/test_files/php_unit/DatabaseTest.php
@@ -0,0 +1,93 @@
+<?phpy
+
+use phpunit\framework\TestCase;
+require_once('/data/individual_project/php/modules/DatabaseClass.php');
+
+class DatabaseTest extends TestCase {
+  public function test_execute_sql() {
+    $db = new Database();
+    $sql = "SELECT * FROM phpunittest WHERE recordtime = '2016-07-27 00:00:26'";
+    $result = $db->execute_sql($sql)->fetchAll(PDO::FETCH_ASSOC);
+    $this->assertEquals('3077', $result[0]['stopid']);
+    $this->assertEquals('20160725108720', $result[0]['uniqueid']);
+    $this->assertNotEquals('1234', $result[0]['stopid']);
+    $this->assertCount(8, $result[0]); // 8 columns, 1 row
+    $this->assertArrayHasKey('stopid', $result[0]);
+    $this->assertArrayHasKey('visitnumber', $result[0]);
+    $this->assertArrayHasKey('destinationtext', $result[0]);
+    $this->assertArrayHasKey('vehicleid', $result[0]);
+    $this->assertArrayHasKey('estimatedtime', $result[0]);
+    $this->assertArrayHasKey('expiretime', $result[0]);
+    $this->assertArrayHasKey('recordtime', $result[0]);
+    $this->assertArrayHasKey('uniqueid', $result[0]);
+  }
+
+  public function test_get_connection() {
+    $db = new Database();
+    $DBH = $db->get_connection();
+    $sql = "SELECT * FROM phpunittest WHERE recordtime = '2016-07-27 00:00:26'";
+
+    try {
+      $database_obj = $DBH->prepare($sql);
+      $database_obj->execute();
+    }
+    catch(PDOException $e) {
+      echo $e->getMessage();
+    }
+
+    $result = $database_obj->fetchAll(PDO::FETCH_ASSOC);
+    $this->assertEquals('3077', $result[0]['stopid']);
+    $this->assertEquals('20160725108720', $result[0]['uniqueid']);
+    $this->assertNotEquals('1234', $result[0]['stopid']);
+    $this->assertCount(8, $result[0]); // 8 columns, 1 row
+    $this->assertArrayHasKey('stopid', $result[0]);
+    $this->assertArrayHasKey('visitnumber', $result[0]);
+    $this->assertArrayHasKey('destinationtext', $result[0]);
+    $this->assertArrayHasKey('vehicleid', $result[0]);
+    $this->assertArrayHasKey('estimatedtime', $result[0]);
+    $this->assertArrayHasKey('expiretime', $result[0]);
+    $this->assertArrayHasKey('recordtime', $result[0]);
+    $this->assertArrayHasKey('uniqueid', $result[0]);
+  }
+
+  /**
+   * @expectedException "PDOException"
+  */
+  public function testTypeException() {
+    $db = new Database("", "", "bus_data", 
+    	      	       "", ""); // no type
+  }
+
+  /**
+   * @expectedException "PDOException"
+  */
+  public function testIPException() {
+    $db = new Database("pgsql", "", "bus_data", 
+    	      	       "", ""); // no IP
+  }
+
+  /**
+   * @expectedException "PDOException"
+  */
+  public function testNameException() {
+    $db = new Database("pgsql", "", "", 
+    	      	       "", ""); // no database
+  }
+
+  /**
+   * @expectedException "PDOException"
+  */
+  public function testUsernameException() {
+    $db = new Database("pgsql", "", "bus_data", 
+    	      	       "", ""); // no username
+  }
+
+  /**
+   * @expectedException "PDOException"
+  */
+  public function testPasswordException() {
+    $db = new Database("pgsql", "", "bus_data", 
+    	      	       "", ""); // no password
+  }
+  
+}
