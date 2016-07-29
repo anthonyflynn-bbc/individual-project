78dc6bc9defc6b2bb216993cd3376f4318916478
Anthony Flynn <anthonymilesflynn@gmail.com>
2016-07-29 09:47:11 +0100

    Added default parameters and access modifiers to HttpClientClass.  Added unit tests for HttpClientClass.php

--- php_scripts/modules/HttpClientClass.php
+++ php_scripts/modules/HttpClientClass.php
@@ -1,37 +1,46 @@
 <?php
 
 class HttpClient {
-  protected $username = "LiveBus77966";
-  protected $password = "ChUdA6weye";
-  protected $curl; // curl resource
-  protected $url; // URL from which to source data
-  protected $fp; // file pointer for output data
+  private $username;
+  private $password;
+  private $url; // URL from which to source data
+  private $fp; // file pointer for output data
+  private $curl; // curl resource
 
   // Constructor
-  function __construct($url, $fp) {
+  public function __construct($url, $fp, $username = "",
+  	   	       $password = "") {
     $this->url = $url;
     $this->fp = $fp;
+    $this->username = $username;
+    $this->password = $password;
     $this->curl = curl_init();
     $this->set_options();
   }
 
-  // Function to set relevant options (e.g. username, password, connection timeout etc.)
-  function set_options() {
+  // Function to set relevant options (e.g. username, password, timeout etc.)
+  private function set_options() {
     curl_setopt($this->curl, CURLOPT_URL, $this->url);
-    curl_setopt($this->curl, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST); // Digest authorisation
+    curl_setopt($this->curl, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST); // authorisation
     curl_setopt($this->curl, CURLOPT_USERPWD, $this->username.":".$this->password);
-    curl_setopt($this->curl, CURLOPT_TIMEOUT, 99999999); // Long-lived connection
-    curl_setopt($this->curl, CURLOPT_FILE, $this->fp);
+    curl_setopt($this->curl, CURLOPT_TIMEOUT, 99999999); //Long-lived connection
+    curl_setopt($this->curl, CURLOPT_FILE, $this->fp); //save data to file pointer
   }
 
   // Function to start session (begin data collection)
-  function start_data_collection() {
-    curl_exec($this->curl);
+  public function start_data_collection() {
+    if(curl_exec($this->curl) === false) { // error connecting to url
+      echo "Error collecting data.\n";
+      return false;
+    }
+    echo "Data collection started.\n";
+    return true;
   }
 
   // Function to close session
-  function close_connection() {
-    curl_close($this->curl);
+  public function close_connection() {
+    curl_close($this->curl); // no return value
+    echo "Curl connection closed.\n";
   }
 }
 ?>
