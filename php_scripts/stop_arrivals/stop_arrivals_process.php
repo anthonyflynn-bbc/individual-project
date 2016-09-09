95df1c6542ea4dca0a5e985b8f6242214c2c4f33
Anthony Flynn <anthonymilesflynn@gmail.com>
2016-09-09 11:39:31 +0100

    Added final version of the stop arrivals process including description / summary at top and change to include statement

--- php_scripts/stop_arrivals/stop_arrivals_process.php
+++ php_scripts/stop_arrivals/stop_arrivals_process.php
@@ -1,6 +1,12 @@
 <?php
 
-include '/data/individual_project/php/modules/StopArrivalsClass.php';
+// stop_arrivals_process.php
+// Anthony Miles Flynn
+// (8/9/16)
+// Process for instantiating the StopArrivals class extracting stop arrivals
+// from stop prediction data.  Run each hour on data 5-6 hours in the past.
+
+include_once ('/data/individual_project/php/modules/StopArrivalsClass.php');
 
 $current_time = time(); // record time of start of program execution
 $start_time = $current_time  - 6 * 60 * 60; // 6 hours prior to current time
